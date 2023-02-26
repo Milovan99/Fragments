031465e6536a76240454ab0f0749c87c0e1ede2d
Milovan99 <milovanjakovljevic78@gmail.com>
2023-02-26 19:56:43 +0100

    Initial commit

---
+++ app/src/main/java/com/milovanjakovljevic/first_fragment/BlankFragment.java
@@ -0,0 +1,23 @@
+package com.milovanjakovljevic.first_fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class BlankFragment extends Fragment {
+
+    public BlankFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_blank, container, false);
+    }
+}
